55a0beb2d903960d83e58a89aaa800cb94917642
LughGK <radiception@gmail.com>
2020-05-05 19:54:15 -0400

    first commit

---
+++ src/main/java/net/lostfables/lughgk/lostprofessions/Lostprofessions.java
@@ -0,0 +1,17 @@
+package net.lostfables.lughgk.lostprofessions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lostprofessions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
